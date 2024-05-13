b069932b5c0592780897fb97bf75422fe8cbc8f6
Ravita Prajapati <89205202+RavitaP12345@users.noreply.github.com>
2024-05-13 16:39:20 +0530

    Uploaded By Ravita on 13-05-2024

---
+++ micro-services-combo-example/micro-service-master/src/test/java/com/example/microservicemaster/MicroServiceMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
